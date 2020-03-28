ff1a8b253968a6f7fc6a4800280442da075931a8
Ko Zin <www.zinwine.mm@gmail.com>
2020-03-29 01:02:14 +0630

    Factory and Seed

--- app/User.php
+++ app/User.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use App\Question;
+use App\Model\Question;
 
 class User extends Authenticatable
 {
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,10 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Model\Category;
+use App\Model\Question;
+use App\Model\Reply;
+use App\Model\Like;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +16,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(App\User::class, 10)->create();
+        factory(Category::class, 5)->create();
+        factory(Question::class, 15)->create();
+        factory(Reply::class, 50)->create()->each(function($reply){
+            return $reply->like()->save(factory(Like::class)->make());
+        });
     }
 }

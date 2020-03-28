e82178309a3cd288e095cf0c31ea3192583cc78a
Ko Zin <www.zinwine.mm@gmail.com>
2020-03-29 00:28:52 +0630

    Relationships

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -3,8 +3,22 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use App\User;
 
 class Question extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hsaMany(Reply::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Reply::class);
+    }
 }

559849458b2e41d579d43c062116ea1b527785cf
Ko Zin <www.zinwine.mm@gmail.com>
2020-03-29 15:59:43 +0630

    QuestionReosurce Setup

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Model\Question;
 use Illuminate\Http\Request;
+use App\Http\Resources\QuestionResource;
 
 class QuestionController extends Controller
 {
@@ -14,17 +15,7 @@ class QuestionController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return QuestionResource::collection(Question::latest()->get());
     }
 
     /**
@@ -35,7 +26,8 @@ class QuestionController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // auth()->user()->question()->create($request->all());
+        Question::create($request->all());
     }
 
     /**
@@ -46,18 +38,7 @@ class QuestionController extends Controller
      */
     public function show(Question $question)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Model\Question  $question
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Question $question)
-    {
-        //
+        return new QuestionResource($question);
     }
 
     /**
@@ -80,6 +61,7 @@ class QuestionController extends Controller
      */
     public function destroy(Question $question)
     {
-        //
+        $question->delete();
+        return response('Deleted', 201);
     }
 }
--- app/Model/Question.php
+++ app/Model/Question.php
@@ -7,6 +7,15 @@ use App\User;
 
 class Question extends Model
 {
+    // protected $fillable = [
+    //     'title', 'slug', 'body', 'category_id', 'user_id'
+    // ];
+    protected $guarded = [];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
     public function user()
     {
         return $this->belongsTo(User::class);
@@ -20,5 +29,10 @@ class Question extends Model
     public function category()
     {
         return $this->belongsTo(Reply::class);
+    }
+
+    public function getPathAttribute()
+    {
+        return asset("api/question/$this->slug");
     }
 }
